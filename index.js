49ec7a2dcbb410d957bff32ab3ec36bdcac26407
NDev.fr <ndev@ik.me>
2025-02-05 15:32:52 +0000

    feat: add endpoint /api/whoami

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// normal endpoint of API
+app.get('/api/whoami', function (req, res) {
+  const headers = req.headers;
+  console.log(req);
+  res.json({ 
+    "ipaddress": headers["x-forwarded-for"],
+    "language": headers["accept-language"],
+    "software": headers["user-agent"],
+   });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
